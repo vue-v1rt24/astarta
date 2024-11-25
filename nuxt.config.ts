8b1dcb477df93acfd4745c72d2f08787993c6de3
Роман <rublevrus@mail.ru>
2024-11-25 11:31:18 +0300

    Добавление настроек

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ru',
+      },
+    },
+  },
+
+  css: ['assets/css/index.css'],
+
+  router: {
+    options: {
+      linkActiveClass: 'active',
+      linkExactActiveClass: 'active',
+    },
+  },
+
+  experimental: {
+    typedPages: true,
+  },
+
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+});
